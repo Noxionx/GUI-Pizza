c0c5bde1086b1df58d7322ae3b7fcee19594b580
Noxionx <nox35400@gmail.com>
2015-01-05 16:36:41 +0100

    Start ng-repeat pizza

--- app/js/controllers/pizzaCtrl.js
+++ app/js/controllers/pizzaCtrl.js
@@ -1,4 +1,11 @@
 define(["angular", "angular-material", "angular-route", "./controllers"],function(){
     angular.module("controllers").controller("pizzaCtrl",["$scope", function($scope){
+    	$scope.pizzas = [
+    		{"name":"royale","ingredients":["tomate","champignon","lardon","mozzarella"]},
+
+    		{"name":"savoyarde","ingredients":["tomate","champignon","lardon","mozzarella"]},
+
+    		{"name":"4 formage","ingredients":["tomate","champignon","lardon","mozzarella"]}
+    	];
     }]);
 });
