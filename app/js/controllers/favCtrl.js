7bf06c8f28a3e158ca69ed8f516e8bf4d4274810
smed <medard.sylvain@gmail.com>
2015-01-05 18:25:01 +0100

    add body page

--- app/js/controllers/favCtrl.js
+++ app/js/controllers/favCtrl.js
@@ -1,5 +1,27 @@
 define(["angular", "angular-material", "angular-route", "./controllers"],function(){
     angular.module("controllers").controller("favCtrl",["$scope", function($scope){
+    	$scope.pizzas = [
+    		{"name":"Royale","ingredients":["tomate","champignon","lardon","mozzarella"]},
+
+    		{"name":"Savoyarde","ingredients":["tomate","champignon","lardon","mozzarella"]},
+
+    		{"name":"4 formage","ingredients":["tomate","champignon","lardon","mozzarella"]}
+    		];
+    	$scope.types = [
+	      {name:'épaisse'},
+	      {name:'fine'},
+	      {name:'normale'}
+   		 ];
+   		$scope.nbpersonnes = [
+	      {nb:'1'},
+	      {nb:'2'},
+	      {nb:'3'},
+	      {nb:'4'},
+	      {nb:'5'}
+   		 ];
+
+   		$scope.nbPers = $scope.nbpersonnes[1];
+   		$scope.typePate = $scope.types[1];
     }]);
 });
 
--- app/js/controllers/pizzaCtrl.js
+++ app/js/controllers/pizzaCtrl.js
@@ -1,11 +1,28 @@
 define(["angular", "angular-material", "angular-route", "./controllers"],function(){
     angular.module("controllers").controller("pizzaCtrl",["$scope", function($scope){
     	$scope.pizzas = [
-    		{"name":"royale","ingredients":["tomate","champignon","lardon","mozzarella"]},
+    		{"name":"Royale","ingredients":["tomate","champignon","lardon","mozzarella"]},
 
-    		{"name":"savoyarde","ingredients":["tomate","champignon","lardon","mozzarella"]},
+    		{"name":"Savoyarde","ingredients":["tomate","champignon","lardon","mozzarella"]},
 
-    		{"name":"4 formage","ingredients":["tomate","champignon","lardon","mozzarella"]}
-    	];
+    		{"name":"4 formage","ingredients":["tomate","champignon","lardon","mozzarella"]},
+
+        {"name":"Bolognaise","ingredients":["tomate","champignon","lardon","mozzarella"]},
+
+        {"name":"Marocco","ingredients":["tomate","champignon","lardon","mozzarella"]}
+    		];
+    	$scope.types = [
+	      {name:'épaisse'},
+	      {name:'fine'},
+	      {name:'normale'}
+   		 ];
+   		$scope.nbpersonnes = [
+	      {nb:'1'},
+	      {nb:'2'},
+	      {nb:'3'},
+	      {nb:'4'},
+	      {nb:'5'}
+   		 ];
+    	
     }]);
 });
